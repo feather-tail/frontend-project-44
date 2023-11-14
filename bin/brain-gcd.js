8c657700f0092b95471dc4f64e4a36eb32964454
Рыжков <imeliaslake@gmail.com>
2023-11-14 14:15:57 +0300

    add game brain-gcd

---
+++ bin/brain-gcd.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+
+import launchBrainGCD from '../src/games/brain-gcd.js';
+
+launchBrainGCD();
---
+++ src/games/brain-gcd.js
@@ -0,0 +1,24 @@
+import runGame from '../index.js';
+
+const generateGCD = () => {
+  const randNumFirst = Math.floor(Math.random() * 10) + 1;
+  const randNumSecond = Math.floor(Math.random() * 10) + 1;
+  const question = `${randNumFirst} ${randNumSecond}`;
+  let correctAnswer = '';
+  if (randNumSecond % randNumFirst === 0) {
+    correctAnswer = String(randNumFirst);
+    return { question, correctAnswer };
+  }
+  for (let i = 1; i <= randNumFirst / 2; i += 1) {
+    if (randNumSecond % i === 0 && randNumFirst % i === 0) {
+      correctAnswer = String(i);
+    }
+  }
+  return { question, correctAnswer };
+};
+
+const launchBrainGCD = () => {
+  runGame(generateGCD, 'Find the greatest common divisor of given numbers.');
+};
+
+export default launchBrainGCD;
