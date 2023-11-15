fdd80c99f211448265f7bceb0d69eb2781b0e2dc
Рыжков <imeliaslake@gmail.com>
2023-11-15 15:13:36 +0300

    separate getCorrectAnswer function

--- src/games/brain-even.js
+++ src/games/brain-even.js
@@ -1,9 +1,11 @@
 import { launchGame, getRandomNumber } from '../index.js';
 
+const getCorrectAnswer = (num) => num % 2 === 0;
+
 const generateEven = () => {
   const randNum = getRandomNumber(1, 10);
   const question = `${randNum}`;
-  const correctAnswer = randNum % 2 === 0 ? 'yes' : 'no';
+  const correctAnswer = getCorrectAnswer(randNum) ? 'yes' : 'no';
 
   return { question, correctAnswer };
 };
